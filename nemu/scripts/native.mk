9c4e2568e86d56c4985cfaf6d566880e8fc13b05
MiaoHao-oops <haomiao19@mails.ucas.ac.cn>
2022-01-23 18:22:41 +0800

    add count target in native.mk

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -26,10 +26,13 @@ gdb: run-env
 	$(call git_commit, "gdb")
 	gdb -s $(BINARY) --args $(NEMU_EXEC)
 
+count:
+	@echo "Total lines: $$(find $$NEMU_HOME -name "*.[ch]" | xargs cat| grep -v ^$$ | wc -l)(except empty lines)"
+
 clean-tools = $(dir $(shell find ./tools -maxdepth 2 -mindepth 2 -name "Makefile"))
 $(clean-tools):
 	-@$(MAKE) -s -C $@ clean
 clean-tools: $(clean-tools)
 clean-all: clean distclean clean-tools
 
-.PHONY: run gdb run-env clean-tools clean-all $(clean-tools)
+.PHONY: run gdb run-env clean-tools clean-all $(clean-tools) count
