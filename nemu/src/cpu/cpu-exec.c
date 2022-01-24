5df199362f1db353fb2d58f7f45c2bfab4f114d2
MiaoHao-oops <haomiao19@mails.ucas.ac.cn>
2022-01-24 08:22:36 +0800

    fix a bug when watch pc and the program end

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -29,7 +29,9 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
   if (check_wp(_this->pc) == true) {
-    nemu_state.state = NEMU_STOP;
+    if (nemu_state.state == NEMU_RUNNING) {
+      nemu_state.state = NEMU_STOP;
+    }
   }
 }
 
